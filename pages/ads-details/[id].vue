899074c570f16824ba31cefdce23c9386402124d
“rahul.r” <rahul.r@pacewisdom.com>
2025-03-26 14:13:48 +0530

    favorite added for details page

--- pages/ads-details/[id].vue
+++ pages/ads-details/[id].vue
@@ -53,8 +53,8 @@
             <div >
               <div class="card-body">
                 <div class="d-flex justify-content-between">
-                    <h3>{{ restaurantAd.title }}{{restaurantAd.is_favourite}}</h3>
-                    <div class="heart-icon" @click="toggleFavorite(restaurantAd.id, restaurantAd.is_liked)">
+                    <h3>{{ restaurantAd.title }}</h3>
+                    <div class="heart-icon" @click="toggleFavorite(restaurantAd.id, restaurantAd.is_favourite)">
                 <svg v-if="restaurantAd.is_favourite" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
                   <path d="M24.3282 4.99269C20.9761 2.93654 18.0505 3.76514 16.293 5.08501C15.5722 5.6262 15.212 5.89679 15 5.89679C14.788 5.89679 14.4277 5.6262 13.707 5.08501C11.9495 3.76514 9.02386 2.93654 5.6718 4.99269C1.27259 7.69118 0.27715 16.5936 10.4244 24.1043C12.3571 25.5348 13.3235 26.25 15 26.25C16.6765 26.25 17.6429 25.5348 19.5756 24.1043C29.7229 16.5936 28.7274 7.69118 24.3282 4.99269Z" fill="#47509B"/>
                 </svg>
@@ -221,13 +221,13 @@ const toggleFavorite = async (adId, currentStatus) => {
     );
 
     if (response.data) {
-      // Update the is_liked status in the ad ref
-      ad.value.is_liked = !currentStatus;
+      // Update the is_favourite status in the restaurantAd ref
+      restaurantAd.value.is_favourite = !currentStatus;
     }
   } catch (error) {
     console.error("Error toggling favorite:", error);
     // Revert the UI state if the API call fails
-    ad.value.is_liked = currentStatus;
+    restaurantAd.value.is_favourite = currentStatus;
   }
 };
 
