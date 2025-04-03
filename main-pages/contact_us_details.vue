669d2de94a9b0528d26ee851b6501ff3684ba336
“rahul.r” <rahul.r@pacewisdom.com>
2025-04-03 15:04:32 +0530

    support@ohtel.in

--- main-pages/contact_us_details.vue
+++ main-pages/contact_us_details.vue
@@ -8,7 +8,7 @@
 <path d="M2.5 6L9.41302 9.91697C11.9616 11.361 13.0384 11.361 15.587 9.91697L22.5 6" stroke="#161C2D" stroke-width="1.5" stroke-linejoin="round"/>
 <path d="M2.51577 13.4756C2.58114 16.5412 2.61383 18.0739 3.74496 19.2094C4.87608 20.3448 6.45033 20.3843 9.59883 20.4634C11.5393 20.5122 13.4607 20.5122 15.4012 20.4634C18.5497 20.3843 20.1239 20.3448 21.2551 19.2094C22.3862 18.0739 22.4189 16.5412 22.4842 13.4756C22.5053 12.4899 22.5053 11.5101 22.4842 10.5244C22.4189 7.45886 22.3862 5.92609 21.2551 4.79066C20.1239 3.65523 18.5497 3.61568 15.4012 3.53657C13.4607 3.48781 11.5393 3.48781 9.59882 3.53656C6.45033 3.61566 4.87608 3.65521 3.74495 4.79065C2.61382 5.92608 2.58114 7.45885 2.51576 10.5244C2.49474 11.5101 2.49475 12.4899 2.51577 13.4756Z" stroke="#161C2D" stroke-width="1.5" stroke-linejoin="round"/>
 </svg></span>
-          <a href="mailto:support@companyname.com" class="email-link">support@companyname.com</a>
+          <a href="mailto:support@ohtel.in" class="email-link">support@ohtel.in</a>
         </div>
         <p class="description">
           Have any questions or need assistance? We're here to help! Simply reach out to us via email, and we'll get back to you as soon as possible.
